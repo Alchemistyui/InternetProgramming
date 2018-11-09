b094ca8d725457e6aa448f5ed58a3be5e4ffd9d4
Alchemist <1319525622@qq.com>
2018-11-09 22:27:47 +0800

    client

---
+++ multiprocess_client.c
@@ -0,0 +1,39 @@
+#include "unp.h"
+
+void str_cli(FILE* stdin, int socketFd){
+    char sendLine[MAXLINE], recvLine[MAXLINE];
+
+    while(fgets(sendLine, MAXLINE, stdin) != NULL){
+        write(socketFd, sendLine, sizeof(sendLine));
+
+        if(readline(socketFd, recvLine, MAXLINE) == 0){
+            err_quit("readline == 0");
+        }
+        fputs(recvLine, stdout);
+    }
+}
+
+
+int main(int argc, char **argv){
+    int socketFd;
+    struct sockaddr_in servAddr;
+
+    if (argc != 3){
+        err_quit("argc != 3");
+    }
+
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+
+    connect(socketFd, (SA *) &servAddr, sizeof(servAddr));
+
+    str_cli(stdin, socketFd);
+
+    exit(0);
+}
+
+// gcc -o echo_client echo_client.c -lunp
