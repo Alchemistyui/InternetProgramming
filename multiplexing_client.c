97638341d10702bd6882e1b558c3c14984188444
Alchemist <1319525622@qq.com>
2018-11-17 10:27:22 +0800

    add multiplexing client

---
+++ multiplexing_client.c
@@ -0,0 +1,34 @@
+#include "unp.h"
+#include<unistd.h> 
+
+
+
+int main(int argc, char **argv){
+    int childpid;
+
+    if (argc != 3){
+        err_quit("argc != 3");
+    }
+
+    for(int i = 0; i < atoi(argv[2]); i++) {
+        // int c = atoi(argv[2]);
+        // printf("c\n");
+        childpid = fork();
+        if (childpid < 0) // pid都是大于等于0的
+        { 
+            /* error occurred */
+            fprintf(stderr,"Fork Failed!");
+            exit(-1);
+        }
+        if(childpid == 0) {
+            // printf("c\n");
+            const char *v={argv[1]};
+            execlp("/home/alchemist/multiplexing_single_client", v, NULL);// 在子进程中加载指定的可执行文件
+            // printf("emm\n");
+        }
+    }
+    return 0;
+}
+
+// gcc -o multiprocess_client multiprocess_client.c -lunp
+// ./multiprocess_client 127.0.0.1 3
---
+++ multiplexing_single_client.c
@@ -0,0 +1,45 @@
+#include "unp.h"
+// #include<unistd.h> 
+
+void str_cli(FILE* stdin, int socketFd){
+   char sendLine[MAXLINE], recvLine[MAXLINE];
+
+    while(fgets(sendLine, MAXLINE, stdin) != NULL){
+        write(socketFd, sendLine, sizeof(sendLine));
+
+        if(readline(socketFd, recvLine, MAXLINE) == 0){
+            err_quit("readline == 0");
+        }
+        fputs(recvLine, stdout);
+    }
+}
+
+
+
+int main(int argc, char **argv){
+    int socketFd;
+    struct sockaddr_in servAddr;
+
+    
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    // printf("socket\n");
+    bzero(&servAddr, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(SERV_PORT);
+    // printf("emm\n");
+    // printf("1 %s\n", argv[0]);
+    // printf("2 %s\n", argv[1]);
+    // printf("3 %s\n", argv[2]);
+    //多重的时候为0，单个为0
+    // printf("single\n");
+    inet_pton(AF_INET, argv[0], &servAddr.sin_addr);
+    // printf("5\n");
+    connect(socketFd, (SA *) &servAddr, sizeof(servAddr));
+    
+    str_cli(stdin, socketFd);
+    
+
+    exit(0);
+}
+
+// gcc -o echo_client echo_client.c -lunp
